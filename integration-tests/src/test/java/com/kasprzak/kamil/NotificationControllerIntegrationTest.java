7782351b4206ca8df7d58a0bdd10bf49d4993d67
kamilkasprzak <kamilkasprzak1999@gmail.com>
2026-02-11 14:36:21 +0100

    fix integration test

--- integration-tests/src/test/java/com/kasprzak/kamil/NotificationControllerIntegrationTest.java
+++ integration-tests/src/test/java/com/kasprzak/kamil/NotificationControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(properties = {
         "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration,org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration"
 })
+@ActiveProfiles("test")
 public class NotificationControllerIntegrationTest {
 
     @Autowired
